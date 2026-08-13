40f6d733e7ed7f1be9a97e7d222fd28c083efd64
gitnickolson <nickolsonick@gmail.com>
2026-08-13 10:35:12 +0200

    Add more common fields to `server_preferences` table

--- migrations/002_create_preferences_table.rb
+++ migrations/002_create_preferences_table.rb
@@ -9,6 +9,9 @@ Sequel.migration do
       string :birthday_celebration_channel_id, null: true
       integer :max_text_level, null: true
       integer :max_voice_level, null: true
+      string :timezone, default: 'Europe/Berlin'
+      string :locale, default: 'DE'
+      string :server_color, default: '#8a43ff'
     end
 
     alter_table(:server_preferences) do
