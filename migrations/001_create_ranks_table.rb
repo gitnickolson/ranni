ba2e7d10ffb5db6d7a0fade9d3f67a16d1ec914a
gitnickolson <nickolsonick@gmail.com>
2026-08-13 16:47:00 +0200

    Add `text_levels` table

--- migrations/001_create_ranks_table.rb
+++ migrations/001_create_ranks_table.rb
@@ -4,13 +4,14 @@ Sequel.migration do
   change do
     create_table(:ranks) do
       primary_key :id
+      String :server_id, null: false
       String :role_id, null: false
       Integer :required_level, null: false
     end
 
     alter_table(:ranks) do
-      add_index :role_id, name: :unique_role_id, unique: true
-      add_index :required_level, name: :unique_required_level, unique: true
+      add_index %i[server_id role_id], name: :unique_role_id_per_server, unique: true
+      add_index %i[server_id required_level], name: :unique_required_level_per_server, unique: true
     end
   end
 end
