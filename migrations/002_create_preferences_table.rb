a523e9c9e15cab5726c24e618ab1d533a1de9721
gitnickolson <nickolsonick@gmail.com>
2026-08-14 12:23:46 +0200

    Add `TextLevelingEnabled` column to preferences

--- migrations/002_create_preferences_table.rb
+++ migrations/002_create_preferences_table.rb
@@ -8,11 +8,12 @@ Sequel.migration do
       String :birthday_role_id, null: true
       String :birthday_celebration_channel_id, null: true
       String :level_up_congratulation_channel_id, null: true
+      Boolean :text_leveling_enabled, default: false, null: false
       Integer :max_text_level, default: 100, null: false
       Integer :max_voice_level, default: 100, null: false
-      String :timezone, default: 'Europe/Berlin'
-      String :locale, default: 'DE'
-      String :server_color, default: '#8a43ff'
+      String :timezone, default: 'Europe/Berlin', null: false
+      String :locale, default: 'DE', null: false
+      String :server_color, default: '#8a43ff', null: false
     end
 
     alter_table(:server_preferences) do
