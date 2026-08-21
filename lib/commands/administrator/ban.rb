c2e751a488b4b3152ee058d1db07cbff08962aa5
gitnickolson <nickolsonick@gmail.com>
2026-08-21 14:38:59 +0200

    Add `/ban` command

---
+++ lib/commands/administrator/ban.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+module Commands
+  module Administrator
+    class Ban < Command
+      NAME = :ban
+      DESCRIPTION = 'Ban a user'
+      PARAMETERS = [{ type: :user, name: :user, required: true,
+                      description: 'Choose a user' },
+                    { type: :string, name: :reason, required: false,
+                      description: 'Specify a reason' }].freeze
+      ONE_DAY = 86_400
+
+      private
+
+      def command_action
+        member = server_service.member_from(identifier: event.options['user'])
+
+        return transmitter.error_response(event:, text: t('commands.administrator.ban.admin_ban')) if admin_ban?(member)
+
+        reason = event.options['reason']&.capitalize
+        send_dm_with_reason(member, reason) if reason
+
+        display_name = server_service.display_name(user_id: member.id, full: true)
+        member.ban(message_seconds: ONE_DAY, reason:)
+
+        transmitter.response(event:,
+                             text: t('commands.administrator.ban.success_response',
+                                     { display_name: }))
+      end
+
+      def admin_ban?(member)
+        permission_checker.administrator?(user: member)
+      end
+
+      def send_dm_with_reason(member, reason)
+        transmitter.send_message(channel: member.pm,
+                                 text: t('commands.administrator.ban.ban_dm',
+                                         { server_name: server.name, reason: }))
+      end
+    end
+  end
+end
