def67efbab15bcc96529eff8e5b5413bc93baf48
gitnickolson <nickolsonick@gmail.com>
2026-08-28 13:50:21 +0200

    Add `tickets_enabled` column to `server_preferences` table

--- migrations/010_preferences_add_ticket_channel_columns.rb
+++ migrations/010_preferences_add_ticket_channel_columns.rb
@@ -5,6 +5,7 @@ Sequel.migration do
     alter_table(:server_preferences) do
       add_column :ticket_category_id, String, null: true
       add_column :ticket_log_channel_id, String, null: true
+      add_column :tickets_enabled, :boolean, default: false, null: false
     end
   end
 end
